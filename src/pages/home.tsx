dedc9e77067b9dd7cde08a3b2299d1b2624fb609
Bello Adedapo <adedapobello@gmail.com>
2026-09-21 10:38:51 +0100

    welcome

---
+++ src/pages/home.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <div>
+      <div className="min-h-screen bg-clip-text bg-linear-to-br from-purple-500 to-purple-800 text-transparent text-center text-2xl font-bold p-4">
+        Welcome Group 60 Members, To The Employee Leave Management App
+      </div>
+    </div>
+  )
+}
